71543e6488f3b19f995206171cded5b828064ce9
xenx96 <genius.crack41@gmail.com>
2020-12-09 08:37:38 +0000

    a

--- routes/company.js
+++ routes/company.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const xml2js = require('xml2js') // xml 파싱 모듈
 //const { isLoggedIn, isNotLoggedIn } = require('./middleware');
 const Company = require('../schemas/company');
+const axios = require('axios');
 
-
-router.post('/register', async (req, res, next) => {
+//Register for Client
+router.post('/register',async (req, res, next) => {
   //req. Body for Insert Data.
   const {NA, CNU, CNA, PN, MN, PW, PW2} = req.body;
-  console.log(req.body);
+ const CNU_CK  = await postCRN(CNU);
+  if(CNU_CK == false){
+    return  res.render('register',{NA,CNA,PN,MN, CNU ,ErrMsg : "잘못된 사업자 등록번호 입니다!"});
+  }else{
   try {
     const exCNU = await Company.findOne({"CNU" : parseInt(CNU)});
     //const exPN = await Company.findOne({"PN" : parseInt(PN)});
@@ -36,9 +41,44 @@ router.post('/register', async (req, res, next) => {
     console.error(error);
     return next(error);
   }
+  }
 });
 
 
+
+// Company Number check
+
+
+async function postCRN(crn){
+  const postUrl = "https://teht.hometax.go.kr/wqAction.do?actionId=ATTABZAA001R08&screenId=UTEABAAA13&popupYn=false&realScreenId="
+  const xmlRaw = "<map id=\"ATTABZAA001R08\"><pubcUserNo/><mobYn>N</mobYn><inqrTrgtClCd>1</inqrTrgtClCd><txprDscmNo>{CRN}</txprDscmNo><dongCode>15</dongCode><psbSearch>Y</psbSearch><map id=\"userReqInfoVO\"/></map>"
+    try{
+        const result  = await axios.post(postUrl,xmlRaw.replace(/\{CRN\}/, crn),
+        { headers: { 'Content-Type': 'text/xml' } })
+       let CRNumber = await getCRNresultFromXml(result.data);
+       if(CRNumber ==='사업을 하지 않고 있습니다.'){
+         CRNumber = false;
+         
+       }else{
+         CRNumber = true;
+       }
+        console.log(CRNumber)
+        return(CRNumber);
+    }catch(err){
+        console.error(err);
+    }
+        
+}
+
+function getCRNresultFromXml(dataString) {
+    return new Promise((resolve, reject) => {
+        xml2js.parseString(dataString, // API 응답의 'data' 에 지정된 xml 값 추출, 파싱
+            (err, res) => {
+                if (err) reject(err)
+                else resolve(res.map.trtCntn[0]) // trtCntn 이라는 TAG 의 값을 get
+            })
+    })
+}
 
 
 
