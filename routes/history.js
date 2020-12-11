a60ab0cf4617cd7a7ca1845ab4ae35aefccda0f0
root <root@ip-172-26-7-181.ap-northeast-2.compute.internal>
2020-12-11 05:48:13 +0000

    a

--- routes/history.js
+++ routes/history.js
@@ -7,23 +7,4 @@ const moment = require('moment');
 const {isNotLoggedIn} = require('./middleware');
 const router = express.Router();
 
-
-
-
-
-router.get('/history_chart/:_id',isNotLoggedIn ,async (req, res, next) => {
-    const CID = req.decoded.CID;
-    const CNU = req.decoded.CNU;
-    try {
-        const historyone = await History.findOne({"_id" : req.params._id});
-        const processone = await Process.find({"HID" : req.params._id});
-        console.log(historyone);
-        console.log(processone);
-        res.render('history_chart', processone, moment);
-    } catch (err) {
-        console.error(err);
-        next(err);
-    }
-})
-
 module.exports = router;
