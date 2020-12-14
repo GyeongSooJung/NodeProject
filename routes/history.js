a90f6314c789e00b5defe0cebd5dfc2edef4c083
root <root@ip-172-26-7-181.ap-northeast-2.compute.internal>
2020-12-14 06:42:07 +0000

    a

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,6 @@ const express = require('express');
 const History = require('../schemas/history');
 const Car = require('../schemas/car');
 const Device = require('../schemas/device');
-const Process = require('../schemas/process');
 const moment = require('moment');
 const {isNotLoggedIn} = require('./middleware');
 const router = express.Router();
