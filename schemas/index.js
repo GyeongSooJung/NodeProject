369c03f3584c0e73dbd181a7279e9782948e76ec
here4you81 <park.choongbum@gmail.com>
2020-12-23 06:15:56 +0000

    몽고디비 서버주소 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    if(process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== 'production') {
         mongoose.set('debug', true);
     }
-<<<<<<< HEAD
+
     mongoose.connect('mongodb://54.254.0.80:9003/MK', {
-=======
-    mongoose.connect('mongodb://52.79.245.187:9005/MK', {
-        // 13.125.32.71:27017
->>>>>>> 96d1065508ba7f0fa18d2243df670ddb52e2ea91
         dbName: 'MK',
     }, (error) => {
-        if(error) {
+        if (error) {
             console.log('DB Connection is Error', error);
-        } else {
+        }
+        else {
             console.log('DB Connect is SuccessFul!');
         }
     });
