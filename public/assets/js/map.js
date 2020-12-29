a2081e10e9f1db9a3e7060d25659c5848bcfd750
hbc3869 <hbc3869@gmail.com>
2020-12-29 04:30:32 +0000

    a

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -2,64 +2,93 @@ window.onload = function() {
     drawMap('#map');
 };
 
+//지도 그리기
 function drawMap(target) {
-	// SVG 영역 생성
-	var width = 500, height = 500;
-	var svg = d3.select("#map")
-	.append("svg")
-	.attr({"width":width, "height":height});
-	
-	// 파일 읽기
-	d3.json("../assets/ne_10m_admin_1_states_provinces/korea-topo.json", function(err, map) {
-	// 그리기 위한 오브젝트 획득
-	var geo = map.objects["korea-geo"];
-	var map_o = topojson.object(map, geo);
-	// 축척 지정
+    var width = 500, height = 500;
+    var labels;
+
 	var projection = d3.geo.mercator() // projection - 투영법, 메르카토르 투영법 사용
-	.center([127, 35.5])
-	.scale(3500)
-	.translate([width / 2, height / 2]);
-	// 패스 작성
+		.center([127, 35.5])
+		.scale(3500)
+		.translate([width / 2, height / 2]);
 	var path = d3.geo.path()
-	.projection(projection);
-	
-	// SVG에 추가
-	svg.append("path")
-	.datum(map_o)
-	.attr("d", path);
-	
-	// 색 설정
-	svg.selectAll("path").attr("fill", "grey");
-	
-	// 경계선
-	var mesh = topojson.mesh(
-	map, geo,
-	function(a, b) {
-	return a !== b;
-	});
-	
-	svg.append("path")
-	.datum(mesh)
-	.attr("d", path)
-	.attr("class", "boundary");
-	// 지역 이름 표시
-	svg.selectAll(".place-label")
-	.data(map_o.geometries)
-	.enter()
-	.append("text")
-	.attr("class", function(d) {
-	return "place-label";
-	})
-	.attr("transform", function(d) {
-	console.log(d);
-	return "translate(" + path.centroid(d) + ")";
-	})
-	.text(function(d) {
-	var s = d.properties.name_local;
-	if (!s) return;
-	//if (s == "서울특별시") return s;
-	//else return;
-	return s;
-	});
-	});
+		.projection(projection);
+    // var zoom = d3.behavior
+    //     .zoom()
+    //     .translate(projection.translate())
+    //     .scale(projection.scale())
+    //     .scaleExtent([height, 800 * height])
+    //     .on('zoom', zoom);
+
+    var svg = d3
+        .select(target)
+        .append('svg')
+        .attr('width', width + 'px')
+        .attr('height', height + 'px')
+        .attr('id', 'map')
+        .attr('class', 'map');
+
+    var states = svg
+        .append('g')
+        .attr('id', 'states')
+        .call(zoom);
+
+    states
+        .append('rect')
+        .attr('class', 'background')
+        .attr('width', width + 'px')
+        .attr('height', height + 'px');
+
+    //geoJson데이터를 파싱하여 지도그리기
+    d3.json('../assets/ne_10m_admin_1_states_provinces/korea-geo.json', function(json) {
+        states
+            .selectAll('path') //지역 설정
+            .data(json.features)
+            .enter()
+            .append('path')
+            .attr('d', path)
+            .attr('id', function(d) {
+                return 'path-' + d.properties.name_eng;
+            });
+
+        labels = states
+            .selectAll('text')
+            .data(json.features) //라벨표시
+            .enter()
+            .append('text')
+            .attr('transform', translateTolabel)
+            .attr('id', function(d) {
+                return 'label-' + d.properties.name_eng;
+            })
+            .attr('text-anchor', 'middle')
+            .attr('dy', '.35em')
+            .text(function(d) {
+                return d.properties.name;
+            });
+    });
+
+    //텍스트 위치 조절 - 하드코딩으로 위치 조절을 했습니다.
+    function translateTolabel(d) {
+        var arr = path.centroid(d);
+        if (d.properties.code == 31) {
+            //서울 경기도 이름 겹쳐서 경기도 내리기
+            arr[1] +=
+                d3.event && d3.event.scale
+                    ? d3.event.scale / height + 20
+                    : initialScale / height + 20;
+        } else if (d.properties.code == 34) {
+            //충남은 조금 더 내리기
+            arr[1] +=
+                d3.event && d3.event.scale
+                    ? d3.event.scale / height + 10
+                    : initialScale / height + 10;
+        }
+        return 'translate(' + arr + ')';
+    }
+
+    // function zoom() {
+    //     projection.translate(d3.event.translate).scale(d3.event.scale);
+    //     states.selectAll('path').attr('d', path);
+    //     labels.attr('transform', translateTolabel);
+    // }
 }
--- routes/page.js
+++ routes/page.js
@@ -10,7 +10,9 @@ const moment = require('moment');
 const router = express.Router();
 const {isLoggedIn, isNotLoggedIn, DataSet, emailcontrol} = require('./middleware');
 
-
+//----------------------------------------------------------------------------//
+//                                  기본라우터                                //
+//----------------------------------------------------------------------------//
 
 //홈페이지 연결 및 추가 방법 + cookie 연결 사용방법 : company.CNA or company.CNU
 const Route_page= function(page,req,res){
@@ -21,6 +23,20 @@ const Route_page= function(page,req,res){
        res.render(page,{company : req.decoded, aclist});
     });
 }
+
+//기본 페이지 설정
+router.get('/',(req,res,next)=>{
+    res.redirect('main');
+});
+
+// 공통페이지 작성 방법
+Route_page('car_join');
+Route_page('device_join');
+
+//----------------------------------------------------------------------------//
+//                                  회원정보                                  //
+//----------------------------------------------------------------------------//
+
 // 로그인
 router.get('/login',isLoggedIn,(req,res)=>{
     res.render('login',
@@ -53,9 +69,28 @@ router.get('/register',isLoggedIn,emailcontrol,(req,res)=>{
   else {
    return res.render('register',{roadAddrPart1,roadAddrPart2,addrDetail});
   }
-  
 });
 
+//회원정보 수정
+router.get('/profile',isNotLoggedIn,DataSet ,async (req, res, next) => {
+  const CID = req.decoded.CID;
+  const aclist = await Worker.find({"CID" : CID, "AC" : false});
+  
+  try {
+    res.render('profile', {company : req.decoded, aclist});
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//비밀번호 찾기
+router.get('/find', (req,res,next) => {
+  res.render('find');
+});
+
+//----------------------------------------------------------------------------//
+//                                  에러                                      //
+//----------------------------------------------------------------------------//
 
 //ERROR Page
 router.get('/error',(req,res)=>{
@@ -63,48 +98,9 @@ router.get('/error',(req,res)=>{
     {title:'ERROR 404'});
 });
 
-router.get('/',(req,res,next)=>{
-    res.redirect('main');
-});
-
-router.get('/find', (req,res,next) => {
-  res.render('find');
-});
-
-router.get('/device_static',isNotLoggedIn,async(req,res,nex)=>{
-  const CID = req.decoded.CID;
-  const aclist = await Worker.find({"CID" : CID, "AC" : false});
-  
-  const AllCompany = await Company.countDocuments({});
-  const AllHistory = await History.countDocuments({});
-  const AllCar = await Car.countDocuments({});
-  const AllDevice = await Device.countDocuments({});
-  
-  // const companys = await Company.find({"AH" : {$ne: "true"}});
-  // console.log(companys);
-  const company1 = await Company.count({"CK" : "렌터카"});
-  const company2 = await Company.count({"CK" : "카센터"});
-  const company3 = await Company.count({"CK" : "출장정비"});
-  const company4 = await Company.count({"CK" : "출장세차"});
-  const company5 = await Company.count({"CK" : "택시운수업"});
-  const company6 = await Company.count({"CK" : "버스운수업"});
-  const company7 = await Company.count({"CK" : "타이어샵"});
-  const companycount = [company1,company2,company3,company4,company5,company6,company7];
-  console.log(companycount[0]);
-  
-  res.render('device_static',{company : req.decoded, companycount, aclist, AllDevice, AllCar, AllHistory, AllCompany});
-})
-
-router.get('/history_static',isNotLoggedIn,async(req,res,nex)=>{
-  const CID = req.decoded.CID;
-  const aclist = await Worker.find({"CID" : CID, "AC" : false});
-  
-  const AllCompany = await Company.countDocuments({});
-  const AllHistory = await History.countDocuments({});
-  const AllCar = await Car.countDocuments({});
-  const AllDevice = await Device.countDocuments({});
-  res.render('history_static',{company : req.decoded, aclist, AllDevice, AllCar, AllHistory, AllCompany});
-})
+//----------------------------------------------------------------------------//
+//                                  메인                                      //
+//----------------------------------------------------------------------------//
 
 //메인 페이지
 router.get('/main',isNotLoggedIn , async(req,res,next)=>{
@@ -140,14 +136,11 @@ router.get('/main',isNotLoggedIn , async(req,res,next)=>{
     }
 });
 
+//----------------------------------------------------------------------------//
+//                                  사업자                                    //
+//----------------------------------------------------------------------------//
 
-// 공통페이지 작성 방법
-Route_page('car_join');
-Route_page('device_join');
-
-
-////////////////////////////////////////////////////////////////////////////////
-//사업자 목록 페이지
+//사업자 목록
 router.get('/company_list', isNotLoggedIn, async (req, res, next) => {
   
   const CID = req.decoded.CID;
@@ -183,13 +176,55 @@ router.get('/company_list', isNotLoggedIn, async (req, res, next) => {
   }
   else {
     res.render('company_list', {companys, aclist, moment, company : req.decoded});
-    
   }
-  
 })
 
-////////////////////////////////////////////////////////////////////////////////
-//장비 수정 페이지
+//----------------------------------------------------------------------------//
+//                                  통계                                      //
+//----------------------------------------------------------------------------//
+
+//제품 통계
+router.get('/device_static',isNotLoggedIn,async(req,res,nex)=>{
+  const CID = req.decoded.CID;
+  const aclist = await Worker.find({"CID" : CID, "AC" : false});
+  
+  const AllCompany = await Company.countDocuments({});
+  const AllHistory = await History.countDocuments({});
+  const AllCar = await Car.countDocuments({});
+  const AllDevice = await Device.countDocuments({});
+  
+  // const companys = await Company.find({"AH" : {$ne: "true"}});
+  // console.log(companys);
+  const company1 = await Company.count({"CK" : "렌터카"});
+  const company2 = await Company.count({"CK" : "카센터"});
+  const company3 = await Company.count({"CK" : "출장정비"});
+  const company4 = await Company.count({"CK" : "출장세차"});
+  const company5 = await Company.count({"CK" : "택시운수업"});
+  const company6 = await Company.count({"CK" : "버스운수업"});
+  const company7 = await Company.count({"CK" : "타이어샵"});
+  const companycount = [company1,company2,company3,company4,company5,company6,company7];
+  console.log(companycount[0]);
+  
+  res.render('device_static',{company : req.decoded, companycount, aclist, AllDevice, AllCar, AllHistory, AllCompany});
+})
+
+//소독 통계
+router.get('/history_static',isNotLoggedIn,async(req,res,nex)=>{
+  const CID = req.decoded.CID;
+  const aclist = await Worker.find({"CID" : CID, "AC" : false});
+  
+  const AllCompany = await Company.countDocuments({});
+  const AllHistory = await History.countDocuments({});
+  const AllCar = await Car.countDocuments({});
+  const AllDevice = await Device.countDocuments({});
+  res.render('history_static',{company : req.decoded, aclist, AllDevice, AllCar, AllHistory, AllCompany});
+})
+
+//----------------------------------------------------------------------------//
+//                                  장비                                      //
+//----------------------------------------------------------------------------//
+
+//장비 수정
 router.get('/device_edit/:MAC',isNotLoggedIn ,async (req, res, next) => {
   const CID = req.decoded.CID;
   const aclist = await Worker.find({"CID" : CID, "AC" : false});
@@ -202,7 +237,7 @@ router.get('/device_edit/:MAC',isNotLoggedIn ,async (req, res, next) => {
     next(err);
   }
 });
-//Device List Data Setting for Devices
+//장비 목록
 router.get('/device_list', isNotLoggedIn,async (req, res, next) => {
   const CID = req.decoded.CID;
   const aclist = await Worker.find({"CID" : CID, "AC" : false});
@@ -216,8 +251,11 @@ router.get('/device_list', isNotLoggedIn,async (req, res, next) => {
   }
 });
 
-////////////////////////////////////////////////////////////////////////////////
-//차량 수정 페이지
+//----------------------------------------------------------------------------//
+//                                  자동차                                    //
+//----------------------------------------------------------------------------//
+
+//차량 수정
 router.get('/car_edit/:CN',isNotLoggedIn,async (req, res, next) => {
   const CID = req.decoded.CID;
   const aclist = await Worker.find({"CID" : CID, "AC" : false});
@@ -231,7 +269,7 @@ router.get('/car_edit/:CN',isNotLoggedIn,async (req, res, next) => {
   }
 });
 
-//Car List Data Setting for Cars
+//차량 목록
 router.get('/car_list',isNotLoggedIn, async (req, res, next) => {
   const CID = req.decoded.CID;
   const aclist = await Worker.find({"CID" : CID, "AC" : false});
@@ -246,25 +284,11 @@ router.get('/car_list',isNotLoggedIn, async (req, res, next) => {
   }
 });
 
-////////////////////////////////////////////
-//Profile DataSetting for Profile(company)
-router.get('/profile',isNotLoggedIn,DataSet ,async (req, res, next) => {
-  const CID = req.decoded.CID;
-  const aclist = await Worker.find({"CID" : CID, "AC" : false});
-  
-  try {
-    res.render('profile', {company : req.decoded, aclist});
-  } catch (err) {
-    console.error(err);
-  }
-});
+//----------------------------------------------------------------------------//
+//                                  작업자                                    //
+//----------------------------------------------------------------------------//
 
-
-
-////////////////////////////////////
-//worker
-
-//Worker list for Workers
+//작업자 목록
 router.get('/worker_list',isNotLoggedIn, async (req, res, next) => {
   const CID = req.decoded.CID;
   const CNU = req.decoded.CNU;
@@ -310,11 +334,13 @@ router.get('/worker_list',isNotLoggedIn, async (req, res, next) => {
     const workers = await Worker.find({CID : req.decoded.CID,});
   
   res.render('worker_list', {aclist, company : req.decoded, workers});
-  
 });
 
-///////////////////////////////////////
-// History
+//----------------------------------------------------------------------------//
+//                                  소독이력                                  //
+//----------------------------------------------------------------------------//
+
+// 소독 목록
 router.get('/history_list', isNotLoggedIn, async (req, res, next) => {
   const CID = req.decoded.CID;
   const CNU = req.decoded.CNU;
@@ -348,6 +374,7 @@ router.get('/history_list', isNotLoggedIn, async (req, res, next) => {
   }
 });
 
+//소독 그래프
 router.get('/history_chart/:_id',isNotLoggedIn ,async (req, res, next) => {
   const CID = req.decoded.CID;
   const CNU = req.decoded.CNU;
@@ -369,7 +396,10 @@ router.get('/history_chart/:_id',isNotLoggedIn ,async (req, res, next) => {
     }
 })
 
-/////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------//
+//                                  QR코드                                    //
+//----------------------------------------------------------------------------//
+
 //Mobile Connect Page
 router.get('/mobile_con', async (req, res, next) => {
     try {
@@ -380,8 +410,8 @@ router.get('/mobile_con', async (req, res, next) => {
     }
 });
 
-
-
+//----------------------------------------------------------------------------//
+//                                  A/S                                       //
+//----------------------------------------------------------------------------//
 
 module.exports = router;
-
