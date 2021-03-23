373ce1245e12cd2267e7f09af9fdc858f21ea3c4
GyeongSooJung <gsjung06@naver.com>
2021-03-23 02:07:36 +0000

    a

--- routes/page.js
+++ routes/page.js
@@ -21,11 +21,6 @@ const axios = require('axios');
 //----------------------------------------------------------------------------//
 
 //기본 페이지 설정
-<<<<<<< HEAD
-router.get('/', (req, res, next) => {
-  
-  res.redirect('main');
-=======
 router.get('/', isLoggedIn, (req, res) => {
   res.render('index');
 });
@@ -33,7 +28,6 @@ router.get('/', isLoggedIn, (req, res) => {
 router.get('/en', function(req, res) {
   res.cookie('lang', 'en');
   res.redirect('/main');
->>>>>>> 17cfd9590ba9055dc6c8b40c0f0233d4333d29b8
 });
 
 router.get('/ko', function(req, res) {
