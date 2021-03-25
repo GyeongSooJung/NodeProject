3d8ff4ced7843355489eb11533ebf1c492d8e135
GyeongSooJung <gsjung06@naver.com>
2021-03-25 03:47:28 +0000

    a

---
+++ schemas/point.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const PointSchema = new Schema({
+    
+    //Company ID
+    CID: {
+        type: String,
+        required: true,
+    },//Point Name
+    PN: {
+        type: String,
+        unique: true
+    },//Point
+    PO: {
+        type: Number
+    }, //Create Time
+    CA: {
+        type: Date,
+        default: Date.now
+    },
+    
+}, { collection: 'Point' });
+
+module.exports = mongoose.model('Point', PointSchema, 'Point');
