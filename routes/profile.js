b5531c6be7cfa3b25e30001a2aff6dae22ab3fec
root <root@ip-172-26-5-4.ap-southeast-1.compute.internal>
2021-04-21 03:17:15 +0000

    a

--- routes/profile.js
+++ routes/profile.js
@@ -4,65 +4,68 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const {isLoggedIn,isNotLoggedIn,DataSet} = require('./middleware');
 
-
-router.post('/pfupdate',isNotLoggedIn,DataSet,async(req,res,next)=>{
-    const{ CNA, PN, MN, PW} = await req.body;
-    console.log("CNA : " + CNA);
-    console.log("PN : " + PN);
-    console.log("PW : " + PW);
-    console.log(req.decoded.company.PW);
-    console.log(PW);
-    
-    try{
-        
-    if(bcrypt.compareSync( PW,req.decoded.company.PW)){
-            console.log("CNA : " + CNA);
-    console.log("PN : " + PN);
-    console.log("PW : " + PW);
-       const companyone = await Company.where({"_id" : req.decoded.company._id}).update({ "CNA": CNA, "PN":PN }).setOptions({ runValidators: true }).exec();
-       console.log(companyone);
-        return res.redirect('/profile?update=true');
-    }else{
-        return res.redirect('/profile?uperr=true');
-    }
-        
-        }catch(err){
-            console.error(err);
-        }
-    });
-
-
-router.post('/pwchange',isNotLoggedIn,DataSet,async(req,res,next)=>{
-    const {PW,PWc1,PWc2} = await req.body;
-    const company = req.decoded.company;
-    try{
-        console.log('입력'+PW);
-        console.log('현재'+company.PW);
-        if(bcrypt.compareSync(PW, req.decoded.company.PW)){
-            if(PW !==PWc1){
-                if(8 <= PWc1.length){
-                    if(PWc1 === PWc2){
-                        const hash = await bcrypt.hash(PWc1, 12);
-                        await console.log(hash);
-                        await Company.update({"_id" : company._id},{PW : hash, UA : Date.now()});
-                        return res.redirect('/profile?success=true');
-                    }
-                    else{
-                        return res.redirect('/profile?pwcerr=true');
-                    }
-                }else{
-                    return res.redirect('/profile?pwc=true');
-                }
-            }else{
-                return res.redirect('/profile?same=true');
-            }
-        }else{
-            return res.redirect('/profile?pwerr=true');
-        }
-    }catch(err){
-        console.error(err);
-    }
+router.post('/editInfo', isNotLoggedIn, DataSet, async(req, res, next) => {
+   const { CNA, MN, PN, PW } = req.body;
+   const company = req.decoded.company;
+   
+  try {
+      if (bcrypt.compareSync(PW, company.PW)) {
+         const exMnCompany = await Company.findOne({ "MN" : MN });
+         const exPnCompany = await Company.findOne({ "PN" : PN });
+         if (exMnCompany) {
+            return res.send({ status: 'exMn' });
+         }
+         else if (exPnCompany) {
+            return res.send({ status: 'exPn' });
+         }
+         else {
+            await Company.where({ "_id" : company._id }).update({$set : { "CNA" : CNA, "MN" : MN, "PN" : PN, "UA" : Date.now() }});
+            return res.send({ status: 'success' });
+         }
+      } else {
+         return res.send({ status: 'fail' });
+      }
+  } catch(err) {
+      console.error(err);
+      next(err);
+  }
 });
 
+router.post('/emailCK', isNotLoggedIn, DataSet, async(req, res, next) => {
+   const { EA } = req.body;
+   const company = req.decoded.company;
+   
+   try {
+      if (EA == company.EA) {
+         return res.send({ status: 'match' });
+      }
+      else {
+         return res.send({ status: 'mismatch' });
+      }
+   } catch(err) {
+      console.error(err);
+      next(err);
+   }
+});
+
+router.post('/editPw', isNotLoggedIn, DataSet, async(req, res, next) => {
+   const { PW, CPW, RPW } = req.body;
+   const company = req.decoded.company;
+   
+  try {
+     console.log("확인"+PW+CPW+RPW);
+      if (bcrypt.compareSync(PW, company.PW)) {
+         const hashCPW = await bcrypt.hash(CPW, 12);
+         await Company.where({ "_id" : company._id }).update({$set : { "PW" : hashCPW, "UA" : Date.now() }});
+         await res.cookie("token", req.cookies,{expiresIn:0});
+         return res.send({ status: 'success' });
+      } else {
+         return res.send({ status: 'fail' });
+      }
+  } catch(err) {
+      console.error(err);
+      next(err);
+  }
+});
 
 module.exports = router;
